e1e407e3a17ee9e82916b762704aa564d4b7fc51
hectorlr <hectorlr@me.com>
2013-09-14 10:26:01 -0600

    Initial Commit

---
+++ Eat ZoomTests/Eat_ZoomTests.m
@@ -0,0 +1,34 @@
+//
+//  Eat_ZoomTests.m
+//  Eat ZoomTests
+//
+//  Created by Hector Rodriguez on 9/14/13.
+//  Copyright (c) 2013 Hector Rodriguez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Eat_ZoomTests : XCTestCase
+
+@end
+
+@implementation Eat_ZoomTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
